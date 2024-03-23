1568e5f61aa472c2c296e0b360dc47d9581608e7
tomas6446 <tomas.kozakas@gmail.com>
2024-03-23 20:50:17 +0200

    Add interpreter

--- src/main/java/org/os/core/Loader.java
+++ src/main/java/org/os/core/Loader.java
@@ -2,28 +2,45 @@ package org.os.core;
 
 import org.os.vm.VirtualMachine;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 public class Loader {
-    private CommandEnum[] commands = CommandEnum.values();
+    public void load(MemoryManager memoryManager, File file, VirtualMachine virtualMachine) {
+        try {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
 
-    public VirtualMachine load(RealMachine realMachine, File file, VirtualMachine virtualMachine) {
-        return virtualMachine;
+                String line;
+                int address = 0;
+                while ((line = reader.readLine()) != null) {
+                    List<String> args = List.of(line.split(" "));
+                    CommandEnum command = CommandEnum.valueOf(args.getFirst());
+
+                    if (command == CommandEnum.MOVE) {
+                        List<String> moveArgs = List.of(args.get(1).split(","));
+                        int regValue1 = Integer.parseInt(moveArgs.get(0));
+                        int regValue2 = Integer.parseInt(moveArgs.get(1));
+
+                        memoryManager.write(virtualMachine.getPtr() + address, command.getCode(), virtualMachine.getPtr());
+                        memoryManager.write(virtualMachine.getPtr() + address + 1, regValue1, virtualMachine.getPtr());
+                        memoryManager.write(virtualMachine.getPtr() + address + 2, regValue2, virtualMachine.getPtr());
+                        address += 3;
+                        continue;
+                    }
+                    memoryManager.write(virtualMachine.getPtr() + address, command.getCode(), virtualMachine.getPtr());
+                    address++;
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
-
-// PSEUDOCODE:
-
-//DATA_SEGMENT
-//var1 VAL 3
-//var2 VAL 5
-//result VAL 0
-//CODE_SEGMENT
-//LD var1
-//MOVE BR, AR
-//LD var2
-//ADD
-//PT result
-//PRNTC result
-//HALT
+// ADD
+// SUB
+// MOVE 1, AR
